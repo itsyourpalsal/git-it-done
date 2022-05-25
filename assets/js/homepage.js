0f96f455252cd52941f3f6ac19751447299a6d01
Salvador Beltran <salbeltran1995@gmail.com>
2022-05-24 21:36:14 -0400

    list issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -30,18 +30,19 @@ var getUserRepos = function(user) {
     fetch (apiUrl).then(function(response) {
         // request was successful 
         if (response.ok){
+            console.log(response);
             response.json().then(function(data) {
                 console.log(data);
                 displayRepos(data, user);
             });
         } else {
-            alert('Error: ' + response.statusText);
+            alert('Error: GitHub User Not Found');
         }
-    })
-    .catch(function(error) {
-        // Notice this '.catch()' getting chained onto the end of the '.then()' method
-        alert("Unable to connect to Github");
-    });
+        })
+        .catch(function(error) {
+            // Notice this '.catch()' getting chained onto the end of the '.then()' method
+            alert("Unable to connect to Github");
+        });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -61,8 +62,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo" + repoName);
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
