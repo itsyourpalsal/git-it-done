12762966eaf5bfdf326a548e67eb40257c626ce5
Salvador Beltran <salbeltran1995@gmail.com>
2022-06-14 23:09:43 -0400

    language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,8 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("repo-search-term");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -21,6 +22,17 @@ var formSubmitHandler = function(event) {
     } 
 };
 
+var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeatureRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -94,5 +106,22 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var getFeatureRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+
+};
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
